bd1c47322d5cf999097fac4a9273a40de8ccbacf
Nicolas Andreas Tamm Garetto <nicolastamm@hotmail.com.ar>
2022-08-03 00:33:11 +0200

    Update RunModel.cs

--- Assets/Scripts/RunModel.cs
+++ Assets/Scripts/RunModel.cs
@@ -5,8 +5,6 @@ public class RunModel : MonoBehaviour
 {
     private BlingTokenizer tokenizer = null;
 
-    public string _modelPath = "F:/Thesis/Model/onnx/gpt2-10.onnx";
-
     public string _tokenizerBinPath = "F:/Thesis/Model/tokenizer/gpt2.bin";
     public string _tokenizerI2WPath = "F:/Thesis/Model/tokenizer/gpt2.i2w";
 
